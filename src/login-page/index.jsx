081f8db06c79b1b012c294fbfea0d7a5a4a7015d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-11-11 17:04:11 +0100

    Login page skeleton

--- src/login-page/index.jsx
+++ src/login-page/index.jsx
@@ -3,14 +3,44 @@ import React, { Component, PropTypes } from 'react';
 class LoginPage extends Component {
   render() {
     return (
-      <div>
-        {this.constructor.name}
-      </div>
+      <form className="col-sm-6 col-sm-offset-3">
+        <div className="form-group">
+          <label htmlFor="username">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+            id="username"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <div className="checkbox">
+          <label htmlFor="rememberMe">
+            <input
+              type="checkbox"
+              id="rememberMe"
+            />
+            Remember me on this device
+          </label>
+        </div>
+        <button className="btn btn-primary">Sign In</button>
+      </form>
     );
   }
 }
 
 LoginPage.propTypes = {
+  loginAsUser: PropTypes.func.isRequired,
 };
 
 export default LoginPage;
+
+
