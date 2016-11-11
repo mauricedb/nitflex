b55caac47bc0ed9fb12b06df33e798fbac7b6243
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-11-11 19:25:18 +0100

    GenreRowMovie

--- src/main-page/genre-row-movie.jsx
+++ src/main-page/genre-row-movie.jsx
@@ -1,16 +1,33 @@
 import React, { Component, PropTypes } from 'react';
 
 class GenreRowMovie extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { movie } = this.props;
+
     return (
-      <div>
-        {this.constructor.name}
+      <div className="col-sm-2 genre-row-movie">
+        <div className="title">
+          {movie.title}
+        </div>
+        <img
+          className="img-responsive"
+          alt={movie.title}
+          src={`http://image.tmdb.org/t/p/w300/${movie.backdrop_path}`}
+        />
+        <button className="btn btn-link btn-block expand" onClick={this.expandMovie}>
+          <i className="glyphicon glyphicon-chevron-down" />
+        </button>
       </div>
     );
   }
 }
 
 GenreRowMovie.propTypes = {
+  movie: PropTypes.object.isRequired,
 };
 
 export default GenreRowMovie;
