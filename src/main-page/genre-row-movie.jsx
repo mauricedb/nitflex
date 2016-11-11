3843461e2a74e1df36acb2c1a65ec317d2302e41
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-11-11 19:21:05 +0100

    GenreRow

---
+++ src/main-page/genre-row-movie.jsx
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class GenreRowMovie extends Component {
+  render() {
+    return (
+      <div>
+        {this.constructor.name}
+      </div>
+    );
+  }
+}
+
+GenreRowMovie.propTypes = {
+};
+
+export default GenreRowMovie;
--- src/main-page/genre-row.jsx
+++ src/main-page/genre-row.jsx
@@ -1,16 +1,30 @@
 import React, { Component, PropTypes } from 'react';
+import GenreRowMovie from './genre-row-movie';
+import './genre-row.css';
 
 class GenreRow extends Component {
   render() {
+    const { genre, movies, startPlaying } = this.props;
+
     return (
-      <div>
-        {this.constructor.name}
+      <div className="row genre-row">
+        <h2>{genre}</h2>
+
+        {movies.slice(0, 6).map(movie => (
+            <GenreRowMovie
+              key={movie.id}
+              movie={movie}
+              expandMovie={this.expandMovie}
+            />))}
       </div>
     );
   }
 }
 
 GenreRow.propTypes = {
+  genre: PropTypes.string.isRequired,
+  movies: PropTypes.array.isRequired,
+  startPlaying: PropTypes.func.isRequired,
 };
 
 export default GenreRow;
